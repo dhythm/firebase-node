9e537529c40c61a0bd282b2c76474cbaf1641edd
dhythm <Yuta.Okada.20@gmail.com>
2020-04-29 22:59:17 +0900

    update

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,15 @@
 import * as express from "express";
-// import { database } from "firebase-admin";
+// import { admin, database } from "firebase-admin";
 import * as functions from "firebase-functions";
 
 const app = express();
+// admin.initializeApp();
+// const db = database();
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
 app.get("/hello", (_request, response) => {
-  // const db = database();
   // const ref = db.ref("users");
 
   response.send(`Hello from Firebase!`);

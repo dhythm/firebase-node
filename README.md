7beffb0551609310e61858e0d7adb3bb9b6f29e4
dhythm <Yuta.Okada.20@gmail.com>
2020-04-30 11:28:11 +0900

    update

--- README.md
+++ README.md
@@ -15,3 +15,16 @@ npm install --save express
 # Run on local
 npm run serve
 ```
+
+# Add realtime database
+```bash
+firebase init database
+
+# Run on local
+firebase emulators:start --only database
+# GET
+curl 'http://localhost:9000/users/jack/name.json'
+# POST
+curl -X PUT -d '{ "first": "Jack", "last": "Sparrow" }' \
+  'http://localhost:9000/users/jack/name.json'
+```
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,18 +1,28 @@
 import * as express from "express";
-// import { admin, database } from "firebase-admin";
+import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 
+const serviceAccount = require("../../credentials/serviceAccountKey.json");
+
 const app = express();
-// admin.initializeApp();
-// const db = database();
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "http://localhost:9000",
+});
+const db = admin.database();
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
 app.get("/hello", (_request, response) => {
-  // const ref = db.ref("users");
+  const ref = db.ref("/users");
+  // console.log({ ref });
+  ref.once("value", (snapshot) => {
+    console.log(snapshot.val());
+    response.send(`Get data from realtime database.`);
+  });
 
-  response.send(`Hello from Firebase!`);
+  // response.send(`Hello from Firebase!`);
 });
 
 export const api = functions.https.onRequest(app);

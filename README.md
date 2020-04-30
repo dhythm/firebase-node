84c2429cd518c31fbd65a102812a2d1dc9d58589
dhythm <Yuta.Okada.20@gmail.com>
2020-04-30 22:49:47 +0900

    update

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ cd functions
 npm install --save express
 
 # Run on local
+export GOOGLE_APPLICATION_CREDENTIALS="../credentials/serviceAccountKey.json"
 npm run serve
 ```
 
@@ -21,10 +22,14 @@ npm run serve
 firebase init database
 
 # Run on local
+# https://github.com/firebase/firebase-tools/issues/1485#issuecomment-510123852
 firebase emulators:start --only database
 # GET
 curl 'http://localhost:9000/users.json'
 # POST
 curl -X PUT -d '{ "first": "Jack", "last": "Sparrow" }' \
   'http://localhost:9000/users/jack/name.json'
+# OR
+curl -X PUT -d @users.json 'http://localhost:9000/users.json'
 ```
+
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,18 @@
 import * as express from 'express';
 import * as admin from 'firebase-admin';
-import * as functions from 'firebase-functions';
+import { https } from 'firebase-functions';
+
+// process.env.FIREBASE_DATABASE_EMULATOR_HOST = 'localhost:9000';
 
 const app = express();
 
-// const dev = process.env.NODE_ENV !== 'production';
 const serviceAccount = require('../../credentials/serviceAccountKey.json');
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential:
+    // process.env.FUNCTIONS_EMULATOR === 'true'
+    //   ? admin.credential.applicationDefault()
+    //   : admin.credential.cert(serviceAccount),
+    admin.credential.cert(serviceAccount),
   databaseURL:
     process.env.FUNCTIONS_EMULATOR === 'true'
       ? 'http://localhost:9000'
@@ -17,18 +22,22 @@ admin.initializeApp({
         ).databaseURL,
 });
 const db = admin.database();
+// const ref = db.ref('users');
+const ref = db.ref();
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-app.get('/hello', (_request, response) => {
-  const ref = db.ref('users');
-  console.log({ PROCESS_ENV: process.env });
-  console.log({ ref });
+app.get('/hello', (request, response) => {
+  // axios
+  //   .get('http://localhost:9000/users.json')
+  //   .then((res) => console.log({ ...res.data }));
+  // console.log({ ref });
 
   ref.once(
     'value',
     (snapshot) => {
+      response.send(`Hello from Firebase!`);
       console.log(snapshot.val());
     },
     (error) => {
@@ -36,15 +45,15 @@ app.get('/hello', (_request, response) => {
     },
   );
 
-  response.send(`Hello from Firebase!`);
+  // response.send(`Hello from Firebase!`);
 });
 
-app.get('/database', (_request, _response) => {
-  const ref = functions.database.ref('users');
+// app.get('/database', (_request, _response) => {
+//   const ref = functions.database.ref('users');
 
-  ref.onCreate((snapshot, context) => {
-    console.log(snapshot.val());
-  });
-});
+//   ref.onCreate((snapshot, context) => {
+//     console.log(snapshot.val());
+//   });
+// });
 
-export const api = functions.https.onRequest(app);
+export const api = https.onRequest(app);
